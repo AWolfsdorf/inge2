aafb4eb3d9cf9e8bdf15e1ca945537c39abcccfe
Nikita Zdanovitch <nzdanovitch@gmail.com>
2022-04-20 20:14:36 -0300

    extract soot command.

--- taller2/run_soot.bash
+++ taller2/run_soot.bash
@@ -1,28 +1,23 @@
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+set -ex
+
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest1 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest2 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest3 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest4 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest5 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest6 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
-java -jar \
-    zero-analysis/target/zero-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar \
+./soot \
     -cp .:./examples/:$JRE -f J ZeroAnalysisTest7 \
     -v -print-tags -p jtp.DivisionByZeroAnalysis on
